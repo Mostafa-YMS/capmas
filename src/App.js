c9b5b1adbcfce483c8aed803a8d58b40f0d9b985
Mostafa-YMS <mos_302@yahoo.com>
2023-05-18 00:09:43 +0300

    custom theme & rtl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import "./App.css";
+import getDesignTokens from "./theme";
+import Home from "./Pages/Home";
+
 function App() {
-  return <></>;
+  document.body.setAttribute("dir", "rtl");
+
+  const theme = useMemo(() => {
+    return createTheme({
+      direction: "rtl",
+      ...getDesignTokens(),
+    });
+  }, []);
+
+  let plugins = [prefixer];
+  plugins.push(rtlPlugin);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/theme/index.js
@@ -0,0 +1,36 @@
+const getDesignTokens = (mode) => ({
+  typography: {
+    allVariants: {
+      fontFamily: "Cairo",
+      textTransform: "none",
+    },
+  },
+  palette: {
+    primary: {
+      main: "#e9f2ff",
+      background: "#e9f2ff",
+      default: "#e9f2ff",
+      white: "#fff",
+      contrastText: "#000",
+    },
+    secondary: {
+      main: "#fff",
+      background: "#e9f2ff",
+      default: "#fff",
+      contrastText: "#000",
+    },
+    text: {
+      primary: "#000000",
+      secondary: "#FCFCFC",
+      white: "#fff",
+    },
+    background: {
+      default: "#fff",
+      paper: "#fff",
+      main: "#f3f6f9",
+      secondary: "#f9f9fa",
+    },
+  },
+});
+
+export default getDesignTokens;
