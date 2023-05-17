a2e3e22d011ef35610d7aceb40ab83f54b677060
Mostafa-YMS <mos_302@yahoo.com>
2023-05-18 00:10:28 +0300

    vectors

---
+++ src/Assets/index.js
@@ -0,0 +1,6 @@
+import moi from "./vectors/moi.png";
+import moh from "./vectors/moh.svg";
+import male from "./vectors/male.svg";
+import female from "./vectors/female.svg";
+
+export { moi, moh, male, female };
